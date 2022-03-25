d8949fca051b72785a302c85f78c5f6e013427f0
Mino <38447110+MINOMINOMI@users.noreply.github.com>
2022-03-25 14:34:19 +0900

    add texture bulk sizing script in /WIP

--- runs_with_editorUtility/Debug/debug.py
+++ runs_with_editorUtility/Debug/debug.py
@@ -1,22 +1,25 @@
+import struct
 import unreal 
 
-asset_lists :list = unreal.EditorUtilityLibrary.get_selected_assets()
+eal = unreal.EditorAssetLibrary 
 
-wraped_blend_mode = unreal.BlendMode
+asset_lists :list                  = unreal.EditorUtilityLibrary.get_selected_assets()
+asset :object                      = asset_lists[0]
+each :struct                       = asset.materials[0]
+masked :struct                     = unreal.BlendMode.cast(1) # static enum UBlendMode[1] = BlendMode::Masked
+MI :object                         = each.material_interface
+MI_overridable_properties :struct  = MI.get_editor_property('base_property_overrides')
+#MI_lightmass_setting        = MI.get_editor_property('lightmass_settings')
 
-asset = asset_lists[0]
-
-each = asset.materials[0]
-
-MI = each.get_editor_property('material_interface')
-MI_overridable_properties = MI.get_editor_property('base_property_overrides')
 MI_overridable_properties.set_editor_property( 'override_blend_mode', True )
 
-MI_overridable_properties.get_editor_property( 'override_blend_mode')
+blend_mode = MI_overridable_properties.get_editor_property('override_blend_mode')
 
-
-
+MI_overridable_properties.set_editor_property('override_two_sided', True)
+MI_overridable_properties.set_editor_property('two_sided', True)
 
 ### 이거 돌리면 엔진 죽음 why? ###
-MI_overridable_properties.set_editor_property( 'blend_mode', wraped_blend_mode.cast(1) )
+MI_overridable_properties.set_editor_property('blend_mode', masked)
 ### 이거 돌리면 엔진 죽음 why? ###
+#### 무조건 Serialize 해야함  -> PY에 serialize 있나? 
+
