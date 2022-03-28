e307b4912649c43ddd8254745be5d354729809cc
Mino <38447110+MINOMINOMI@users.noreply.github.com>
2022-03-28 12:31:03 +0900

    if parent != unreal.Material() -> do nothing

--- runs_with_editorUtility/Debug/debug.py
+++ runs_with_editorUtility/Debug/debug.py
@@ -1,12 +1,4 @@
 import unreal 
 import re 
+from typing import List
 
-
-re.search(seek, anim_list)
-
-
-for each in anim_list :
-    result = re.search(seek, each)
-    if result != None :
-        print(result)
-        print(each)
