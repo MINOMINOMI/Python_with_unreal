bfc1ed0ac539bbde271d8fb534ab4c4cb2dcfd3d
Mino <38447110+MINOMINOMI@users.noreply.github.com>
2022-04-05 18:30:00 +0900

    code organising

--- runs_with_editorUtility/components/get_HLOD_enabled.py
+++ runs_with_editorUtility/components/get_HLOD_enabled.py
@@ -4,4 +4,6 @@ import unreal
 world_setting   :object   = unreal.EditorLevelLibrary.get_editor_world().get_world_settings()
 is_HLOD_enabled :bool     = world_setting.get_editor_property('enable_hierarchical_lod_system') 
 
-unreal.log_warning(is_HLOD_enabled)
+need_to_print   :str      = 'HLOD enabled = ' + str(is_HLOD_enabled)
+
+unreal.log_warning(need_to_print)
